036e430173d9fff26b3f4620f6128e4e05f2ff14
diksonsamuel <dikson.samuel@sifycorp.com>
2023-08-29 17:36:28 +0530

    conntent changes

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -2,9 +2,9 @@ const Strings = {
   home: "Home",
   work: "Work",
   contact: "Contact",
-  introLine1: "Hello, I’m Dikson Samuel. \nI’m a mobile/frontend developer.",
-  introLine2: "I’m a mobile/frontend developer.",
-  introPara1: "I have 7 years’ experience at mobile and frontend development.",
+  introLine1: "Hello, I’m Dikson Samuel. \nI’m a Mobile/Web developer.",
+  introLine2: "I’m a mobile/web developer.",
+  introPara1: "I possess seven years of experience in mobile and web development.",
   introPara2: "I prioritize clarity, empathy, and integrity above everything else. These principles shape my problem-solving approach and my perspective on life."
 
   // introLine1: "Lorem ipsum dolor sit amet \nLorem ipsum dolor sit amet, consectetur",
