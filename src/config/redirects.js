6d825de9a44534542946c9013ad2ab3788f79b79
Dikson Samuel <diksonsamuel11@gmail.com>
2023-09-07 13:52:01 +0530

    bug fixes

--- src/config/redirects.js
+++ src/config/redirects.js
@@ -1,7 +1,7 @@
 const Redirects = {
   linkedin: "https://www.linkedin.com/in/dikson-samuel-980a21a3/",
   facebook: "https://www.facebook.com/dikson.samuel",
-  instagram: "",
+  instagram: "https://www.instagram.com/diksonsamuel/",
   github: "https://github.com//diksonsamuel",
   twitter: "https://twitter.com/diksonsamuel"
 }
